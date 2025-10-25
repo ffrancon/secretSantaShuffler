43d03a8f9f161102f3bc9e34e3bbf7c40a198f33
Florian Françon <61508413+ffrancon@users.noreply.github.com>
2025-10-25 10:37:08 +0200

    reorganize app & provider

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,11 +1,11 @@
-import { SecretSantaContextProvider } from "./context/provider";
-import { Header } from "./components/ui/Header/Header";
-import { Pairs } from "./components/ui/Pairs/Pairs";
-import { Players } from "./components/ui/Players/Players";
+import { Provider } from "./provider";
+import { Header } from "@/components/ui/Header/Header";
+import { Pairs } from "@/components/ui/Pairs/Pairs";
+import { Players } from "@/components/ui/Players/Players";
 
 function App() {
   return (
-    <SecretSantaContextProvider>
+    <Provider>
       <div className="min-h-screen bg-gradient-to-r from-slate-900 to-slate-800 p-4 text-sm text-slate-200">
         <Header />
         <div className="mx-auto grid w-[80vw] max-w-[1024px] grid-cols-2 gap-3">
@@ -13,7 +13,7 @@ function App() {
           <Pairs />
         </div>
       </div>
-    </SecretSantaContextProvider>
+    </Provider>
   );
 }
 
--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -1,12 +1,8 @@
 import { type ReactNode, useReducer } from "react";
-import { reducer } from "./reducer";
-import { initialState, SecretSantaContext } from "./context";
+import { reducer } from "@/context";
+import { initialState, SecretSantaContext } from "@/context";
 
-export const SecretSantaContextProvider = ({
-  children,
-}: {
-  children: ReactNode;
-}) => {
+export const Provider = ({ children }: { children: ReactNode }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
 
   return (
