9f23842d737902db5a1bfe6f04e269017e813f3e
Florian Françon <61508413+ffrancon@users.noreply.github.com>
2025-10-26 16:56:46 +0100

    implemnt confirm dialog for pairs clear

--- src/components/ui/Pairs/Pairs.tsx
+++ src/components/ui/Pairs/Pairs.tsx
@@ -1,8 +1,9 @@
 import { useSecretSantaCtx } from "@/app/context";
 import { generateRandomPairs } from "@/utils/generateRandomPairs";
-import { memo, useCallback } from "react";
+import { Fragment, memo, useCallback, useState } from "react";
 import { Pair } from "./Pair";
 import { Button } from "@/components/ui/common/Button";
+import { ConfirmDialog } from "../common/ConfirmDialog";
 
 export const Pairs = memo(() => {
   const {
@@ -21,36 +22,56 @@ export const Pairs = memo(() => {
     dispatch({ type: "clear_pairs" });
   }, [dispatch]);
 
+  const [isClearConfirmDialogOpen, setClearConfirmDialogOpen] = useState(false);
+  const openClearDialog = useCallback(
+    () => setClearConfirmDialogOpen(true),
+    [],
+  );
+  const closeClearDialog = useCallback(
+    () => setClearConfirmDialogOpen(false),
+    [],
+  );
+
   return (
-    <div className="flex h-full flex-col rounded-md border border-slate-700 bg-slate-800 shadow-md">
-      <div
-        className={`flex flex-grow flex-col ${
-          pairs.length > 0 ? "" : "items-center justify-center"
-        } divide-y divide-slate-700 overflow-y-auto`}
-      >
-        {pairs.length > 0 ? (
-          pairs.map((pair) => (
-            <Pair
-              key={`pair-${pair[0].replace(/\s+/g, "-")}-${pair[1].replace(/\s+/g, "-")}`}
-              pair={pair}
-            />
-          ))
-        ) : (
-          <p className="text-sm text-slate-400">No pairs generated yet.</p>
-        )}
-      </div>
-      <div className="flex justify-between gap-2 border-t border-slate-700 p-3">
-        <Button onClick={generatePairs} disabled={players.length < 2}>
-          Generate Pairs
-        </Button>
-        <Button
-          variant="destructive"
-          onClick={clearPairs}
-          disabled={pairs.length === 0}
+    <Fragment>
+      <ConfirmDialog
+        isOpen={isClearConfirmDialogOpen}
+        title="Clear all pairs"
+        content="Are you sure you want to clear all pairs?"
+        confirm={clearPairs}
+        close={closeClearDialog}
+        isDestructive
+      />
+      <div className="flex h-full flex-col rounded-md border border-slate-700 bg-slate-800 shadow-md">
+        <div
+          className={`flex flex-grow flex-col ${
+            pairs.length > 0 ? "" : "items-center justify-center"
+          } divide-y divide-slate-700 overflow-y-auto`}
         >
-          Clear all
-        </Button>
+          {pairs.length > 0 ? (
+            pairs.map((pair) => (
+              <Pair
+                key={`pair-${pair[0].replace(/\s+/g, "-")}-${pair[1].replace(/\s+/g, "-")}`}
+                pair={pair}
+              />
+            ))
+          ) : (
+            <p className="text-sm text-slate-400">No pairs generated yet.</p>
+          )}
+        </div>
+        <div className="flex justify-between gap-2 border-t border-slate-700 p-3">
+          <Button onClick={generatePairs} disabled={players.length < 2}>
+            Generate Pairs
+          </Button>
+          <Button
+            variant="destructive"
+            onClick={openClearDialog}
+            disabled={pairs.length === 0}
+          >
+            Clear all
+          </Button>
+        </div>
       </div>
-    </div>
+    </Fragment>
   );
 });
